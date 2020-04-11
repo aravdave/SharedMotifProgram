6c72c9e3196f53d602c52add3aeb1d67d35db775
addition23 <aravdave@gmail.com>
2020-04-11 11:53:49 -0400

    Adjusted to Java naming conventions

--- motifFinder.java
+++ motifFinder.java
@@ -7,7 +7,7 @@ import java.util.*;
 import java.awt.*;
 import javax.swing.JFrame;
 
-public class motifFinder {
+public class MotifFinder {
     public static void main(String[] args) throws FileNotFoundException {
         
         String file1Path, file2Path;
